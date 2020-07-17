95b374f4d4a273ee40937022a8f4658dfc9d82a1
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-16 20:21:57 -0400

    added character scaffold

---
+++ app/views/characters/_character.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! character, :id, :name, :class, :alignment, :species, :description, :created_at, :updated_at
+json.url character_url(character, format: :json)
---
+++ db/migrate/20200717002139_create_characters.rb
@@ -0,0 +1,13 @@
+class CreateCharacters < ActiveRecord::Migration[6.0]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.string :class
+      t.string :alignment
+      t.string :species
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
