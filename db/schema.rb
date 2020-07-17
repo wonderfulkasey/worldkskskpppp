1413ab2236aa9e1c9744062fd3799aaef676fe34
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-16 20:27:47 -0400

    created migration

---
+++ db/schema.rb
@@ -0,0 +1,43 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_07_17_002701) do
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.string "class"
+    t.string "alignment"
+    t.string "species"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.text "worlds"
+    t.text "characters"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "worlds", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.text "aesthetic"
+    t.text "inhabitants"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
