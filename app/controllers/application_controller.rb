c6acc7da1d3b9553ea05bbb5d6163a64dcd8cd49
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-16 21:24:40 -0400

    added cancan stuff to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,6 @@ class ApplicationController < ActionController::Base
 
     before_action :authenticate_user!
 
+    load_and_authorize_resource
+
 end
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -8,8 +8,9 @@ RailsAdmin.config do |config|
   end
       config.current_user_method(&:current_user)
 
+
   ## == CancanCan ==
-  # config.authorize_with :cancancan
+      config.authorize_with :cancancan
 
   ## == Pundit ==
   # config.authorize_with :pundit
