76d395fd974f74fae32e58b74a5c885049b7ce4c
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-16 23:03:46 -0400

    fixing github routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
   
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-   
+    mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+   # resources :users, only: [:show]
     resources :players
     resources :worlds
     resources :characters
   
 
-  root "players#home"
+   root "players#home"
  
   
 
   devise_for :users, controllers: {
-    registrations: "registrations" }
+    registrations: "registrations",
+    omniauth_callbacks: "omniauth_callbacks" 
+  }
     
 
   devise_scope :user do 
@@ -20,11 +22,11 @@ Rails.application.routes.draw do
   end
 
   devise_scope :user do 
-  get 'logout', to: 'devise/sessions#destroy'
+    get 'logout', to: 'devise/sessions#destroy'
   end
 
   devise_scope :user do
-  get 'signup', to: 'devise/registrations#new'
+    get 'signup', to: 'devise/registrations#new'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
