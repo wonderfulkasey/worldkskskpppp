a0f19f0c7f94ad44374a15b577440a7154bca941
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-17 23:31:47 -0400

    applied new merge

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,9 +1,5 @@
 class Character < ApplicationRecord
-<<<<<<< HEAD
     #belongs_to :user
-=======
-    belongs_to :user
->>>>>>> 2b8a38a5d8ef376e94442a83052b4e766c296897
 
     validates :name, length: { minimum: 2 }
 end
